21f948805252d6598fed66bb126e5d33479d849e
Matthew Spire <matthew_spire@yahoo.com>
2019-07-02 16:09:11 -0700

    Built a graph, connected the graph to the UI, and made navigation happen

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 // import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -20,6 +22,16 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
+
+        // The complete onClickListener with Navigation
+        // binding.playButton.setOnClickListener { view: View ->
+        //     view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        // }
+
+        // The complete onClickListener with Navigation using createNavigateOnClickListener
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
         return binding.root
     }
 }
